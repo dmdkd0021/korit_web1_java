3869dc5f65a31e17f83e82ab97ae24e938773997
dmdkd0021 <anjffhgkwl0000@gmail.com>
2025-10-17 21:33:30 +0900

    new : If

---
+++ src/main/java/_05_String/Escape.java
@@ -0,0 +1,26 @@
+package _05_String;
+
+public class Escape {
+    public static void main(String[] args) {
+        /*
+        이스케이프 문자
+        문자열 안에 이스케이프 문자를 사용할 수 있댜.
+        \n : 줄바꿈 - 엔터키와 같은 효과
+        \t : 탭 - 스페이스바 4번 효과
+        \' : 작은따옴표 문자로 인식
+        \" : 큰따옴표 문자로 인식
+        \\ : 백슬래시 문자로 인식
+         */
+
+        System.out.println("집에\n가고\n싶다");
+        System.out.println("이름:\t" + "으아앙");
+        // 파일 경로 c:\Program Files\Java
+        System.out.println("C:\\Program Files\\Java");
+
+
+
+
+
+
+    }
+}
